421c2ab199d45d8a2e4ab8eb7e4020d07a6e6b12
Robert Coffie <rcoffie22@yahoo.com>
2024-09-08 19:54:17 +0000

    adding list view

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,9 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("blog/", include("blog.urls", namespace="blog")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
