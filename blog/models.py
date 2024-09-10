0c7a6978a3e2d05d93355536e206dfe41df2882a
Robert Coffie <rcoffie22@yahoo.com>
2024-09-10 11:19:25 +0000

    adding pagination

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
 from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
 
