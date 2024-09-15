1cc494c19c834807b16e5e1a57394c8917cf8cf6
Robert Coffie <rcoffie22@yahoo.com>
2024-09-15 02:37:19 +0000

    comment-egine

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
+from django.views.generic import ListView
 
-from blog.models import Post
-
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
+from .models import Post
 
 # Create your views here.
 
@@ -22,9 +23,21 @@ def post_list(request):
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-# def post_detail(request, id):
-#     post = get_object_or_404(Post, id=id, status=Post.status.PUBLISHED)
-#     return render(request, "blog/post/detail.html", {"post": post})
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
+    )
 
 
 def post_detail(request, year, month, day, post):
@@ -36,7 +49,9 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, "blog/post/detail.html", {"post": post, "comments":comments,"form":form})
 
 
 def post_share(request, post_id):
@@ -46,19 +61,14 @@ def post_share(request, post_id):
     sent = False
     if form.is_valid():
         cd = form.cleaned_data
-        post_url= request.build_absolute_uri(post.get_absolute_url())
-        subject = (
-            f"{cd['name']} ({cd['email']}) "
-            f"recommends you read {post.title}")
+        post_url = request.build_absolute_uri(post.get_absolute_url())
+        subject = f"{cd['name']} ({cd['email']}) " f"recommends you read {post.title}"
         message = (
             f"Read {post.title} at {post_url}\n\n"
-            f"{cd['name']}\'s comments: {cd['comments']}"
+            f"{cd['name']}'s comments: {cd['comments']}"
         )
         send_mail(
-            subject=subject,
-             message=message,
-             from_email=None,
-             recipient_list=[cd['to']]
+            subject=subject, message=message, from_email=None, recipient_list=[cd["to"]]
         )
         sent = True
     else:
